6051a6efc5cea906e1e063e50a10a5cb1264d8ed
Rickard Sandgren <Rickard.Sandgren@cybercom.com>
2024-01-31 12:53:01 +0100

    added paginator to barrels grid

--- src/components/barrels/barrels-grid.tsx
+++ src/components/barrels/barrels-grid.tsx
@@ -1,46 +1,122 @@
-"use server"
+"use client"
 import type { BarrelWithUser } from "@/db/queries/barrels";
 import BarrelCard from "./barrel-card";
-import { deleteBarrel } from "@/actions";
-import { useFormState } from "react-dom";
-import { Auth } from "@auth/core";
-import { auth } from "@/auth";
+import { Pagination } from "@nextui-org/react";
+import { useState } from "react";
+import { useSession } from "next-auth/react";
 interface BarrelsListProps {
-  fetchPublicData: () => Promise<BarrelWithUser[]>,
-  fetchPrivateData: (id: string) => Promise<BarrelWithUser[]>
+  publicBarrels: BarrelWithUser[],
+  privateBarrels: BarrelWithUser[]
 }
 
-export default async function BarrelsGrid({ fetchPublicData, fetchPrivateData }: BarrelsListProps) {
-  const session = await auth();
-  let publicBarrels = await fetchPublicData();
-  let privateBarrels: BarrelWithUser[] = [];
+export default function BarrelsGrid({ publicBarrels, privateBarrels }: BarrelsListProps) {
+  const [publicBarrelsPage, setPublicBarrelsPage] = useState(1);
+  const [privateBarrelsPage, setPrivateBarrelsPage] = useState(1)
 
-  if (session) {
-    if (session.user?.id)
-      privateBarrels = await fetchPrivateData(session.user?.id);
+  const { data: session, status } = useSession()
+  let sessionStatus = "";
 
-    publicBarrels = publicBarrels.filter(barrel => barrel.userId !== session.user?.id)
+  /*
+  if (session.status === "loading") {
+    sessionStatus = "loading";
+  } else if (session.data?.user) {
+    sessionStatus
+  }*/
+
+  const ITEMS_PER_ROW = 4;
+  const ITEMS_PER_PAGE_PRIVATE = 4;
+  const ITEMS_PER_PAGE_PUBLIC = 8;
+
+  console.log("privateBarrels.length: " + privateBarrels.length)
+
+
+  const numPrivateBarrelPages = Math.ceil(privateBarrels.length / ITEMS_PER_PAGE_PRIVATE);
+  console.log("private barrel pages: " + numPrivateBarrelPages)
+  const numPublicBarrelPages = Math.ceil(publicBarrels.length / ITEMS_PER_PAGE_PUBLIC);
+  console.log("public barrel pages: " + numPublicBarrelPages)
+
+  /*
+    const ROWS_PER_PAGE_PUBLIC = 3;
+    const ROWS_PER_PAGE_PRIVATE = 1;
+  
+  
+  
+    const numPrivateBarrelSlots = Math.ceil(privateBarrels.length / ITEMS_PER_ROW) * ITEMS_PER_ROW;
+    const numPublicBarrelSlots = Math.ceil(publicBarrels.length / ITEMS_PER_ROW) * ITEMS_PER_ROW;
+  
+    const numPrivatePages = Math.ceil(numPrivateBarrelSlots / (ITEMS_PER_ROW * ROWS_PER_PAGE_PRIVATE));
+    const numPublicPages = Math.ceil(numPublicBarrelSlots / ITEMS_PER_ROW * ROWS_PER_PAGE_PUBLIC);
+  */
+
+  //const ITEMS_PER_PAGE = ITEMS_PER_ROW * ROWS_PER_PAGE;
+  /*
+    console.log("is logged in!")
+    function getNumberOfPages() {
+      console.log("hello numpages")
+  
+      let numBarrelSlots = 0; // slots is for compensating if a row is not filled completely with barrels.
+      if (status === "authenticated") {
+        const rowsForPrivateBarrels = Math.ceil(privateBarrels.length / ITEMS_PER_ROW);
+        const rowsForPublicBarrels = Math.ceil(publicBarrels.length / ITEMS_PER_ROW);
+        numBarrelSlots = (rowsForPrivateBarrels + rowsForPublicBarrels) * ITEMS_PER_ROW;
+        console.log("rowsForPrivateBarrels:" + rowsForPrivateBarrels)
+        console.log("rowsForPublicBarrels:" + rowsForPublicBarrels)
+        console.log("numBarrelSlots:" + numBarrelSlots)
+      }
+      else
+        numBarrelSlots = publicBarrels.length;
+  
+      const numPages = Math.ceil(numBarrelSlots / ITEMS_PER_PAGE);
+  
+      return numPages;
+    }
+    */
+
+  const handlePrivatePageChange = (newPage: number) => {
+    setPrivateBarrelsPage(newPage);
+  };
+
+  const handlePublicPageChange = (newPage: number) => {
+    setPublicBarrelsPage(newPage);
+  };
+
+  function getBarrelsForPublicPage() {
+    const startItem = ((publicBarrelsPage - 1) * ITEMS_PER_PAGE_PUBLIC);
+    const pageBarrels = publicBarrels.slice(startItem, startItem + ITEMS_PER_PAGE_PUBLIC);
+    return pageBarrels;
   }
-  console.log("is logged in!")
 
+  function getBarrelsForPrivatePage() {
+    console.log(privateBarrelsPage);
 
+    const startItem = ((privateBarrelsPage - 1) * ITEMS_PER_PAGE_PRIVATE);
+    const pageBarrels = privateBarrels.slice(startItem, startItem + ITEMS_PER_PAGE_PRIVATE);
+    return pageBarrels;
+  }
 
   return <>
-    {session && <>
+    {status === "authenticated" && <>
       <div className="mx-5 mb-5 text-xl">Private Barrels</div>
-      <div className="grid grid-cols-3 gap-0">
-        {privateBarrels.map((barrel) => (
+      <div className="grid grid-cols-4 gap-0">
+        {getBarrelsForPrivatePage().map((barrel) => (
           <BarrelCard key={"card" + barrel.id} barrel={barrel} color={"#BBBBEE"} />
         ))}
+      </div>
+
+      <div className="flex items-center justify-center">
+        <Pagination showControls={true} total={numPrivateBarrelPages} page={privateBarrelsPage} onChange={handlePrivatePageChange} />
       </div>
     </>}
 
     <div className="mx-5 mb-5 text-xl">Public Barrels</div>
-    <div className="grid grid-cols-3 gap-0">
-      {publicBarrels.map((barrel) => (
+    <div className="grid grid-cols-4 gap-0">
+      {getBarrelsForPublicPage().map((barrel) => (
         <BarrelCard key={"card" + barrel.id} barrel={barrel} color={"#BBBBEE"} />
       ))}
     </div>
+
+    <div className="flex items-center justify-center">
+      <Pagination showControls={true} total={numPublicBarrelPages} page={publicBarrelsPage} onChange={handlePublicPageChange} />
+    </div>
   </>
 }
-
